1e62618d3c5047c6f35615d80d073a03dbb8f30a
Niklas Gruhn <niklas@gruhn.me>
2024-07-16 00:52:29 +0200

    tinker eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,8 @@ export default tseslint.config(
   {
     rules: {
       "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
-      "@typescript-eslint/consistent-type-assertions": "error",
+      // FIXME: this rule is not seem to get triggered:
+      "@typescript-eslint/consistent-type-assertions": ["error", { "assertionStyle": "never" }],
     }
   }
 );
